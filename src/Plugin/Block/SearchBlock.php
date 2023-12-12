4e13e2670c324a084df9109781e78fde7c56f2cf
Jason Neumark Mickela <jason@rootid.com>
2023-12-11 17:30:27 -0800

    added id to search form

--- src/Plugin/Block/SearchBlock.php
+++ src/Plugin/Block/SearchBlock.php
@@ -21,12 +21,20 @@ class SearchBlock extends BlockBase {
   public function defaultConfiguration()
   {
     return [
+      'form_id' => 'site-search-form',
       'action_route' => '/search',
     ];
   }
 
   public function blockForm($form, FormStateInterface $form_state) {
     $form = parent::blockForm($form, $form_state);
+
+      $form['form_id'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Form ID'),
+      '#description' => $this->t('The ID to use for the search form, this should be unique for each form.'),
+      '#default_value' => $this->configuration['form_id'],
+    ];
 
     $form['action_route'] = [
       '#type' => 'textfield',
@@ -40,12 +48,14 @@ class SearchBlock extends BlockBase {
 
   public function blockSubmit($form, FormStateInterface $form_state) {
     $this->configuration['action_route'] = $form_state->getValue('action_route');
+    $this->configuration['form_id'] = $form_state->getValue('form_id');
   }
 
   public function build() {
     $build = [
       '#theme' => 'radicati_search_form',
       '#action_route' => $this->configuration['action_route'],
+      '#form_id' => $this->configuration['form_id'],
     ];
     return $build;
   }
