078cffb2c60838721a7bbf7519a61944916356fe
Jason Neumark Mickela <jason@rootid.com>
2026-01-15 17:46:14 -0800

    updated component setting formatter

--- src/Plugin/Field/FieldFormatter/TaxonomySettingFormatter.php
+++ src/Plugin/Field/FieldFormatter/TaxonomySettingFormatter.php
@@ -5,6 +5,7 @@ namespace Drupal\radicati_base\Plugin\Field\FieldFormatter;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\Html;
 
 
 /**
@@ -53,7 +54,30 @@ class TaxonomySettingFormatter extends FormatterBase {
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    return [];
+    $classes = [];
+
+    // Create base setting class to let the styles know this setting is being used.
+    $field_name = substr($items->getName(), 6);
+    // If $field_name starts with setting-, then add a second hyphen after setting.
+    if (str_starts_with($field_name, 'setting_')) {
+      $field_name = str_replace('setting_', 'setting--', $field_name);
+    }
+
+    $setting_class = 'component-setting--' . $field_name;
+    $setting_class = Html::cleanCssIdentifier($setting_class);
+
+    //$classes[] = $setting_class;
+
+    foreach ($items as $item) {
+      $title = $item->entity->getName();
+      $title = strtolower($title);
+      $title = $field_name . "--" . $title;
+      $classes[] = Html::cleanCssIdentifier($title);
+    }
+
+    return [
+      '#markup' => implode(' ', $classes),
+    ];
   }
 
 }
